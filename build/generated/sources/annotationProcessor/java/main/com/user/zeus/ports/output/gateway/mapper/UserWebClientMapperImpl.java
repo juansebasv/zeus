5e20f0d582d3c3a54652af5bc30b493ebeaf582a
Sebastian Vega <jzebas94@gmail.com>
2024-07-26 16:42:30 -0500

    Creating develop branch and updating gitinore file

--- build/generated/sources/annotationProcessor/java/main/com/user/zeus/ports/output/gateway/mapper/UserWebClientMapperImpl.java
+++ build/generated/sources/annotationProcessor/java/main/com/user/zeus/ports/output/gateway/mapper/UserWebClientMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-07-25T12:00:16-0500",
+    date = "2024-07-26T16:40:28-0500",
     comments = "version: 1.5.5.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-8.8.jar, environment: Java 17.0.11 (Amazon.com Inc.)"
 )
 @Component
